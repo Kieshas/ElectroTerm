a03f50134d514fa763deee9efacdf6207250761d
Kieshas <78356092+Kieshas@users.noreply.github.com>
2022-11-03 17:11:56 +0200

    objectify

--- src/event-handler.js
+++ src/event-handler.js
@@ -5,80 +5,97 @@ const mainWin = require('./index');
 
 const mainWindow = mainWin.getMainWin();
 
-let SPort;
-let parser;
-let baudNum;
-let currPort;
+const portHandler = {
+    SPort: null,
+    parser: null,
+    currBaud: null,
+    currPort: null,
 
-ipcMain.handle('updateSizeOnLoad', async () => {
-    let sz = mainWindow.getContentSize();
-    return new Promise((resolve) => {
-      resolve(sz);
-    })
-});
+    set changeBaud(newBaud) {
+        this.currBaud = newBaud;
+    },
+    set changePort(newPort) {
+        this.currPort = newPort;
+    },
+
+    open() {
+        return new Promise((resolve, reject) => { 
+            this.SPort = new SerialPort({ path: this.currPort, baudRate: this.currBaud, hupcl: false, autoOpen: false});
+            this.parser = new ReadlineParser();
+            this.SPort.pipe(this.parser);
+    
+            this.parser.on("data", (line) => {
+                mainWindow.webContents.send('printLn', line);
+                portHandler.parserEvt(line);
+            });
+            this.SPort.open((err) => {
+                if (err == null) {
+                    resolve();
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    },
+    cleanUp() {
+        this.SPort = null;
+        this.parser = null;
+    },
+    parserEvt(line) {
+
+    },
+    restart(args) {
+        if (this.SPort == null) return;
+        this.SPort.set({rts: args[0]}, () => {
+            this.SPort.close(() => {
+                this.cleanUp();
+                this.open();
+            });
+        }); // true for ESP false for STM
+    },
+    close() {
+        if (this.SPort == null) return;
+        this.SPort.close(() => {
+            this.cleanUp();
+        });
+    }
+};
   
-ipcMain.handle('populateDD', async () => {
+ipcMain.handle('populateDD', () => {
     const portList = serPort.SerialPort.list();
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         resolve(portList);
     });
 });
 
-const openPort = () => {
-    return new Promise((resolve, reject) => { 
-        SPort = new SerialPort({ path: currPort, baudRate: baudNum, hupcl: false, autoOpen: false});
-        parser = new ReadlineParser();
-        SPort.pipe(parser);
-
-        parser.on("data", (line) => {
-            mainWindow.webContents.send('printLn', line);
-        });
-        SPort.open((err) => {
-            if (err == null) {
-                resolve();
-            } else {
-                reject(err);
-            }
-        });
-    });
-}
-
-const cleanUpPort = () => {
-    SPort = null;
-    parser = null;
-}
-
 ipcMain.handle('setPrmsAndConnect', (event, args) => { // check promise if COM access denied
-    currPort = args[0];
-    baudNum = Number(args[1]);
-    const retVal = openPort();
+    portHandler.changePort = args[0];
+    portHandler.changeBaud = Number(args[1]);
+    const retVal = portHandler.open();
 
     return new Promise((resolve, reject) => {
         retVal.then(() => {
             resolve();
         }).catch((err) => {
-            cleanUpPort();
+            portHandler.cleanUp();
             reject(err);
         })
     })    
 });
 
 ipcMain.on('disconnectPort', () => {
-    SPort.close();
-    cleanUpPort();
+    portHandler.close();
 });
 
-const restart = () => {
-    SPort.close(() => {
-        SPort = null;
-        parser = null;
-        openPort();
-    });
-}
-
 ipcMain.on('restartEvt', (event, args) => {
-    if (SPort == null) return;
-    SPort.set({rts: args[0]}, restart); // true for ESP false for STM
+    portHandler.restart(args);
+});
+
+ipcMain.handle('updateSizeOnLoad', async () => {
+    let sz = mainWindow.getContentSize();
+    return new Promise((resolve) => {
+      resolve(sz);
+    })
 });
 
   //todo autoresponsus pagal tai ka mato terminale. Cool featuresas
