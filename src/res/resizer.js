ccd03b30762b5581ce756ae20efca569528f4092
Kieshas <78356092+Kieshas@users.noreply.github.com>
2022-11-04 18:09:34 +0200

    actual correct resizer

--- src/res/resizer.js
+++ src/res/resizer.js
@@ -8,8 +8,14 @@ const resizeOutput = (size) => {
     if (size == undefined) return;
     let REMsize = parseInt(getComputedStyle(document. documentElement).fontSize, 10);
     let additionalPXS = ((REMsize * 0.25) * (rowCnt + 1)); // all rows use mb-1 (margin of 0.25rem) so we count the ammount of PX that we need to subtract from contentSize to compensate for margins
-    let height = size[1] - (((rowCnt -1) * 38) + additionalPXS); // subtract calculated px ammount from contentsize (rowcnt - 1 because we dont take textare row into account)
+    let height = 0;
+    document.querySelectorAll(".row").forEach( (row) => { // runs one time at startup
+        if (row.id == "outputRow") return; // exception for output row
+        height += row.offsetHeight;
+    });
+    height = size[1] - (height + additionalPXS);
     output.style.height = height.toString() + "px";
+    height = 0;
 } 
 
 window.ipcRender.receive('resizeEvt', (args) => { resizeOutput(args) });
